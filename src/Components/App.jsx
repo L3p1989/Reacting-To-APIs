484158b4f48cbef231b4e9e583666e3060f70886
Patrick Hogan <phogan110@live.com>
2018-12-18 13:09:29 -0700

    Imported logo; created click function; added logo and button

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,13 +3,14 @@ import PostCard from "./PostCard";
 import "isomorphic-fetch";
 import "es6-promise";
 import { Container, Row, Button } from "reactstrap";
+import logo from "./../Images/logo.png";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       films: [],
-      nowLoad: false
+      requestFilmsLoad: false
     };
   }
 
@@ -19,15 +20,35 @@ class App extends Component {
       .then(films => this.setState({ films }));
   } //grabs films and creates json object named films
 
+  handleFilmsClick(e) {
+    e.preventDefault();
+    this.setState({ requestFilmsLoad: true });
+  }
+
   render() {
+    const requestFilmsLoad = this.state.requestFilmsLoad;
     return (
       <Container>
-        <Row>
-          {this.state.films.map(film => {
-            return <PostCard key={film.id} film={film} />;
-            //maps films json objects array into a JS film object for each films object for PostCard to use
-          })}
-        </Row>
+        {requestFilmsLoad ? (
+          <Row>
+            {this.state.films.map(film => {
+              return <PostCard key={film.id} film={film} />;
+              //maps films json objects array into a JS film object for each films object for PostCard to use
+            })}
+          </Row>
+        ) : (
+          <>
+            <img src={logo} alt="" />
+            <Button
+              block
+              className="w-25"
+              onClick={e => this.handleFilmsClick(e)}
+              type="submit"
+            >
+              Load Films
+            </Button>
+          </>
+        )}
       </Container>
     );
   }
