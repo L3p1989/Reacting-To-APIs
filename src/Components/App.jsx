bdf28d2087fd25dcd2263951d4731f858db0ea7e
Patrick Hogan <phogan110@live.com>
2018-12-18 11:00:48 -0700

    imported isomorphic/es6 and created `componentDidMount`

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import "isomorphic-fetch";
+import "es6-promise";
 
 class App extends Component {
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then(res => res.json())
+      .then(obj => console.log(obj));
+  }
+
   render() {
     return <h1>placeholder</h1>;
   }
