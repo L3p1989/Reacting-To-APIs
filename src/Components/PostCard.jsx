be17345b5a080d2984957095215ec754cc486502
Patrick Hogan <phogan110@live.com>
2018-12-18 12:07:05 -0700

    created variable for 'title' and 'description' to clean code

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Card, CardTitle, CardText, Col } from "reactstrap";
 
 const PostCard = props => {
+  const { title, description } = props.film;
   return (
     <Col sm="6">
       <Card body>
-        <CardTitle>{props.film.title}</CardTitle>
-        <CardText>{props.film.description}</CardText>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
       </Card>
     </Col>
   );
