3954ab7a0a380938443b7a347a26bd7d78119c9c
Patrick Hogan <phogan110@live.com>
2018-12-18 11:48:40 -0700

    created state with `films` array; removed placeholder with film import

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import PostCard from "./PostCard";
 import "isomorphic-fetch";
 import "es6-promise";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      films: []
+    };
+  }
+
   componentDidMount() {
     fetch("https://ghibliapi.herokuapp.com/films")
       .then(res => res.json())
-      .then(obj => console.log(obj));
+      .then(films => this.setState({ films }));
   }
 
   render() {
-    return <h1>placeholder</h1>;
+    return (
+      <>
+        {this.state.films.map(film => {
+          return <PostCard key={film.id} film={film} />;
+        })}
+      </>
+    );
   }
 }
 
