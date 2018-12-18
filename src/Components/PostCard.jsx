b0f712ad68b1440a4b8791e01e50c0d7ab067b37
Patrick Hogan <phogan110@live.com>
2018-12-18 12:01:34 -0700

    imported reactstrap components; set up card to render title and desc.

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Card, CardTitle, CardText, Col } from "reactstrap";
 
 const PostCard = props => {
   return (
-    <>
-      <h1>Placeholder</h1>
-    </>
+    <Col sm="6">
+      <Card body>
+        <CardTitle>{props.film.title}</CardTitle>
+        <CardText>{props.film.description}</CardText>
+      </Card>
+    </Col>
   );
 };
 
